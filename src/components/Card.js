d2c7207f8aa9ccdc2e052687b82e8b68330f4c85
heathornJ Desk <heathornj@gmail.com>
2025-01-27 18:00:33 +0000

    Add state and functionality for changing anims when cards are drawn from Deck

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 // Displays a random question, determined in Deck, as the button text
-function Card({ questions, theme, reset }) {
+function Card({
+  questions,
+  theme,
+  reset,
+  index,
+  useDeckAnimation,
+  handleCardClickAnimation,
+}) {
   // State for checking if card toggled
   const [toggledCard, setToggledCard] = useState(false);
   // State for applying animation
@@ -17,6 +24,10 @@ function Card({ questions, theme, reset }) {
 
   // Toggles faceup/facedown card view, removes/applies animation
   const handleCardClick = () => {
+    // Checks if Card was just drawn from deck, if so changes anim.
+    if (useDeckAnimation[index]) {
+      handleCardClickAnimation(index);
+    }
     /* 
       setAnimate and setTimeout work together here.
       setAnimate determines the animation to play: Out if false, In if true.
@@ -26,7 +37,7 @@ function Card({ questions, theme, reset }) {
       In anim plays.
     */
     setAnimate(false);
-    setTimeout(() => setToggledCard((prev) => !prev), 500);
+    setTimeout(() => setToggledCard((prev) => !prev), 550);
     setTimeout(() => setAnimate(true), 500);
   };
 
@@ -34,7 +45,9 @@ function Card({ questions, theme, reset }) {
     return (
       <div
         className={`card-face-down ${
-          animate
+          useDeckAnimation[index]
+            ? "animate__animated animate__rotateInDownLeft"
+            : animate
             ? "animate__animated animate__flipInY"
             : "animate__animated animate__flipOutY"
         }`}
@@ -46,7 +59,9 @@ function Card({ questions, theme, reset }) {
   return (
     <div
       className={`card ${theme.color} ${
-        animate
+        useDeckAnimation[index]
+          ? "animate__animated animate__rotateInDownLeft"
+          : animate
           ? "animate__animated animate__flipInY"
           : "animate__animated animate__flipOutY"
       }`}
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -7,12 +7,18 @@ function Deck({ theme, questions }) {
   const [selectedQuestions, setSelectedQuestions] = useState(["", ""]);
   // State to track resetkey for setting Card to facedown.
   const [resetKey, setResetKey] = useState(0);
+  // State to track what animation type should be used on Card components;
+  const [useDeckAnimation, setUseDeckAnimation] = useState([true, true]);
 
   // Generates 2 random numbers to select an index from the questions array. Adds these questions to the array.
   const handleDrawCards = () => {
     // Increments resetKey causing update on Card
     setResetKey((prevKey) => prevKey + 1);
 
+    // Draw Card using draw animation
+    setUseDeckAnimation([true, true]);
+
+    //Select to randomised indexes to use for
     const maxRange = questions.length;
     let index1 = randomNumberInRange(maxRange);
     let index2 = randomNumberInRange(maxRange);
@@ -37,6 +43,15 @@ function Deck({ theme, questions }) {
   }, [selectedQuestions]);
  */
 
+  // Sets useDeckAnimation state for relevant card
+  const handleCardClickAnimation = (index) => {
+    setUseDeckAnimation((prev) => {
+      const newAnimations = [...prev];
+      newAnimations[index] = false;
+      return newAnimations;
+    });
+  };
+
   return (
     <div className="deck-card-container">
       <div
@@ -44,8 +59,22 @@ function Deck({ theme, questions }) {
         onClick={handleDrawCards}
       ></div>
       <div className="cards-container">
-        <Card questions={selectedQuestions[0]} theme={theme} reset={resetKey} />
-        <Card questions={selectedQuestions[1]} theme={theme} reset={resetKey} />
+        <Card
+          questions={selectedQuestions[0]}
+          theme={theme}
+          reset={resetKey}
+          index={0}
+          useDeckAnimation={useDeckAnimation}
+          handleCardClickAnimation={handleCardClickAnimation}
+        />
+        <Card
+          questions={selectedQuestions[1]}
+          theme={theme}
+          reset={resetKey}
+          index={1}
+          useDeckAnimation={useDeckAnimation}
+          handleCardClickAnimation={handleCardClickAnimation}
+        />
       </div>
     </div>
   );
